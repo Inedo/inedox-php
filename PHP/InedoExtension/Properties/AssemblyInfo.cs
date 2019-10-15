c4f63f4fe40d3c70e57c40cc7601d9bf45c1d738
Alex Papadimoulis <apapadimoulis@inedo.com>
2019-10-15 16:18:08 +0900

    Update AssemblyInfo.cs

--- PHP/InedoExtension/Properties/AssemblyInfo.cs
+++ PHP/InedoExtension/Properties/AssemblyInfo.cs
@@ -1,9 +1,19 @@
-﻿using System.Reflection;
+  
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Inedo.Extensibility;
 
 [assembly: AssemblyTitle("PHP")]
 [assembly: AssemblyDescription("Contains operations to help with CI/CD with PHP-based applications.")]
 [assembly: AssemblyCompany("Inedo")]
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyCompany("Inedo, LLC")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2019")]
+
+[assembly: CLSCompliant(false)]
+[assembly: ComVisible(false)]
 
 [assembly: AppliesTo(InedoProduct.BuildMaster | InedoProduct.Otter | InedoProduct.Hedgehog)]
