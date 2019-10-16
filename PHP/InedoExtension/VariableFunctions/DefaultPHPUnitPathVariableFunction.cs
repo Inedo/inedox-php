ad1374ac4701cdd61553ae97c3cdc2748268018a
Ben Lubar <ben.lubar@gmail.com>
2019-10-16 14:37:56 -0500

    Fix documentation for default PHPUnit path

--- PHP/InedoExtension/VariableFunctions/DefaultPHPUnitPathVariableFunction.cs
+++ PHP/InedoExtension/VariableFunctions/DefaultPHPUnitPathVariableFunction.cs
@@ -5,7 +5,7 @@ using Inedo.Extensibility.VariableFunctions;
 namespace Inedo.Extensions.PHP.VariableFunctions
 {
     [ScriptAlias("PHPUnitPath")]
-    [Description("Full path of phpunit. The default is /usr/bin/phpunit on Linux and ??? on Windows.")]
+    [Description("Full path of phpunit. The default is /usr/bin/phpunit on Linux and C:\\bin\\phpunit.phar on Windows.")]
     [ExtensionConfigurationVariable(Required = false)]
     public sealed class DefaultPHPUnitPathVariableFunction : ScalarVariableFunction
     {
