073dcac0262b351b868eabb7b5aefd6a7bc42b4d
Greg Divis <gdivis@inedo.com>
2024-10-24 10:41:08 -0400

    update sdk

--- PHP/InedoExtension/Properties/AssemblyInfo.cs
+++ PHP/InedoExtension/Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using Inedo.Extensibility;
 [assembly: AssemblyTitle("PHP")]
 [assembly: AssemblyDescription("Contains operations to help with CI/CD with PHP-based applications.")]
 [assembly: AssemblyCompany("Inedo")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
 
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
